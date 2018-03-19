65f65e30b8086a0fc930428bf0defc25866668d3
GnomeGuy <32271984+GnomeGuy@users.noreply.github.com>
2018-03-19 01:57:17 +0100

    Terminado con todo

--- src/Contador.java
+++ src/Contador.java
@@ -1,11 +1,11 @@
 import org.junit.jupiter.api.AfterEach;
 
-    public class Contador {
+public class Contador {
 
-        static int contador = 0;
+    static int contador = 0;
 
-        @AfterEach
-        void afterEach() {
-            System.out.println("Pruebas totales realizadas: " + ++contador);
-        }
+    @AfterEach
+    void afterEach() {
+        System.out.println("Pruebas totales realizadas: " + ++contador);
     }
+}
